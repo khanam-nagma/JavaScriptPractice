7e35cf87a1c2b97e0ea1429becd257615391fb66
khanam-nagma <nagma.nk.khanam@gmail.com>
2025-09-09 19:26:08 +0530

    createElement

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
-
-function changeColor(newColor){
-    const text = document.getElementById("para")
-    text.style.color = newColor;
+function addElement() {
+  const newDiv = document.createElement("div2");
+  const newContect = document.createTextNode(
+    "The document.createElement() method in JavaScript creates a new HTML element with a specified tag name."
+  );
+  newDiv.appendChild(newContect);
+  const newElem = document.querySelector("#id");
+  document.body.insertBefore(newDiv, newElem);
 }
-
-document.querySelectorAll("button").forEach((button) => {
-    button.addEventListener("click", (event) => {
-        changeColor(event.target.textContent.toLowerCase())
-    })
-})
-
-const log = document.querySelector("#log")
-
-function random(number){
-    return Math.floor(Math.random() * number)
-}
-
-function setBackgroundColor(id){
-    log.textContent = "";
-
-    try{
-        const element = document.querySelector(`#${id}`);
-        console.log(element, id)
-        const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-    element.style.backgroundColor = randomColor;
-    }
-    catch (e) {
-    log.textContent = e;
-  }
-}
-
-document.querySelector("#no-escape").addEventListener("click", () => {
-    setBackgroundColor("this?elem")
-});
-document.querySelector("#css-escape").addEventListener("click", () => {
-     setBackgroundColor(CSS.escape("this?elem"));
-})
-document.querySelector("#manual-escape").addEventListener("click", () => {
-      setBackgroundColor("this\\?elem")
-})
+addElement();
